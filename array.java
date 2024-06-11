20e2a02b8df0c6000170a8c979772c3af45c79f8
baibhavkr <68650329+baibhavkr@users.noreply.github.com>
2024-06-11 15:24:58 +1000

    Changed

--- array.java
+++ array.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class array {
 public static void main(String[] args) {
-    int [] mynum={1,2,3,4,5,6};
+    int [] mynum={1,2,3,4,5,6,7};
     System.out.println(mynum);
     String[] cars={"BMW","Volvo","hundai","Maruti"};
     cars[2]="Omni";
