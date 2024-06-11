738d0827194c932c6fe805295b9df2c51a2024e6
Baibhav Kumar <baibhav.kumar@westpac.com.au>
2024-06-11 13:50:55 +1000

    Done

--- array.java
+++ array.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class array {
 public static void main(String[] args) {
     int [] mynum={1,2,3,4};
@@ -5,5 +8,21 @@ public static void main(String[] args) {
     String[] cars={"BMW","Volvo","hundai","Maruti"};
     cars[2]="Omni";
     System.out.println(cars[2]);
+
+int []my_array1 = {1789, 2035, 1899, 1456, 2013, 
+1458, 2458, 1254, 1472, 2365, 
+1456, 2165, 1457, 2456};
+
+String []my_string= {"Java","Python","PHP","C#","C Programming","C++"};
+
+    System.out.println("Original Array sort:"+ Arrays.toString(my_array1));
+
+    Arrays.sort(my_array1);
+    System.out.println("Sorted Array format:"+ Arrays.toString(my_array1));
+
+    System.out.println("Original String sort:"+ Arrays.toString(my_string));
+
+    Arrays.sort(my_string);
+    System.out.println("Sorted Array format"+ Arrays.toString(my_string));
 }
 }
