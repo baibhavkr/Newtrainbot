4e35a0421f92c694b74c678182f35a5750f76b07
Baibhav Kumar <baibhav.kumar@westpac.com.au>
2024-06-11 14:33:15 +1000

    Done

--- array.java
+++ array.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
     System.out.println(mynum);
     String[] cars={"BMW","Volvo","hundai","Maruti"};
     cars[2]="Omni";
-    System.out.println(cars[2]);
+    System.out.println(cars[3]);
 
 int []my_array1 = {1789, 2035, 1899, 1456, 2013, 
 1458, 2458, 1254, 1472, 2365, 
