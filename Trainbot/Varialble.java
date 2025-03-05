d9b7ed59a190a5123fd586ef9394bbe842eb98bd
baibhavkr <68650329+baibhavkr@users.noreply.github.com>
2025-03-05 15:14:35 +1100

    push

---
+++ Trainbot/Varialble.java
@@ -0,0 +1,10 @@
+public class Variable.java
+public static void main(String args[])
+{
+    a=10, b=20, c=30;
+
+    System.out.println("This is a"+a);
+    System.out.println("This is b"+b);
+
+    System.out.println(a+b+c);
+}
